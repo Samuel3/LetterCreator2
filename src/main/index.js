d583b8c01f5b5a1246505bb512aa1f66f9d523dc
samuel <samuel.mathes@gmx.de>
2020-05-09 11:07:11 +0200

    Initial commit for LetterCreator2

---
+++ src/main/index.js
@@ -0,0 +1,96 @@
+'use strict'
+
+import { app, BrowserWindow, ipcMain, shell } from 'electron'
+const os = require('os')
+const path = require('path')
+const fs = require('fs')
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 563,
+    useContentSize: true,
+    width: 1000
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+ipcMain.on('print', () => {
+  console.log('Print')
+  const options = { pageSize: 'A4' }
+  mainWindow.webContents.print(options, (success, errorType) => {
+    if (!success) {
+      console.log(errorType)
+    }
+  })
+})
+
+ipcMain.on('print-to-pdf', function (event) {
+  const pdfPath = path.join(os.tmpdir(), 'print.pdf')
+  var win = BrowserWindow.fromWebContents(event.sender)
+  // Use default printing options
+  win.webContents.printToPDF({pageSize: 'A4'}, function (error, data) {
+    if (error) throw error
+    fs.writeFile(pdfPath, data, function (error) {
+      if (error) {
+        throw error
+      }
+      shell.openExternal('file://' + pdfPath)
+      event.sender.send('wrote-pdf', pdfPath)
+    })
+  })
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
---
+++ src/renderer/components/LandingPage/AdressDialog.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="addressDialog">
+    <h1 id="title">Adress Dialog</h1>
+      !!!{{storedAddresses}}!!!
+    <div v-for="(address) in addresses">
+        {{address.firstName}}
+        {{address.name}}
+    </div>
+  </div>
+</template>
+
+<script>
+    import draggable from 'vuedraggable'
+    const store = require('data-store')('LetterCreator')
+    store.set('address', {'test': 'test'})
+
+    export default {
+      name: 'AdressDialog',
+      components: {
+        draggable
+      },
+      data: function () {
+        return {
+          'addresses': [{'name': 'Name', 'firstName': 'firstName'}],
+          'storedAddresses': store.get('address')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+    @media print {
+        #title{
+            display: none;
+        }
+    }
+
+    #addressDialog{
+        display: block;
+        border: 1px solid;
+    }
+
+</style>

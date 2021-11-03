77bd80191a3006d6d55ed109644aef21cf19082d
Samuel Mathes <samuel.mathes@gmx.de>
2021-11-03 20:25:34 +0100

    Added language persistance

--- src/components/LangChooser.vue
+++ src/components/LangChooser.vue
@@ -1,3 +1,4 @@
+
 <template>
   <v-col cols="6">
     {{$t('menu.language')}}:
@@ -14,14 +15,38 @@
 </template>
 
 <script>
+
 export default {
   name: "i18n",
   data: () => ({
     languages: ['de', 'en']
   }),
+  props: {
+    "language": String
+  },
   methods: {
     changeLang(lang) {
-      this.$i18n.locale = lang
+      this.$i18n.locale = lang;
+      window.ipcRenderer.send("write","app.language", lang)
+    },
+    mounted: function () {
+      window.ipcRenderer.on("print-pdf-path", (event, args) => {
+        console.log("Received print path" + JSON.stringify(args))
+      });
+      window.ipcRenderer.on("app.language", (event, args) => {
+        console.log("Received response for app.language dsf " + args)
+      });
+      window.ipcRenderer.send('read', 'app.language')
+    }
+
+  },
+  watch: {
+    // whenever question changes, this function will run
+    language: {
+      handler(newVal) {
+        this.$i18n.locale = newVal
+      },
+      deep: true
     }
   }
 }

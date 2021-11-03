ece5553b0d62f9d0ccf5fdfda107ebae8d7507f8
Samuel Mathes <samuel.mathes@gmx.de>
2021-11-03 18:51:00 +0100

    Added Sender Component

---
+++ src/components/Sender.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div id="sender" @click="dialog=true">
+      {{renderedSender}}
+    </div>
+    <v-dialog
+        v-model="dialog"
+    >
+      <v-card>
+        <v-card-title>Title</v-card-title>
+        <v-card-text>Text</v-card-text>
+        <v-card-actions>
+          Actions
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sender",
+  props: ['sender'],
+  data: () => ({
+    dialog: false,
+    renderedSender: "Samuel Mathes - Brucknerstr. 28 - 72766 Reutlingen"
+  }),
+  watch: {
+    'sender': function (newSender) {
+      this.renderedSender = newSender
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+#sender {
+  text-decoration: underline;
+  font-size: 10px;
+}
+</style>
